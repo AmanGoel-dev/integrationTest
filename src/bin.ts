8fdbcc1bb342b588965e6734b4a6c20011bb8e6f
AmanGoel-Dev <goelaman79060@gmail.com>
2025-03-20 11:41:29 +0530

    ci/cd

---
+++ src/bin.ts
@@ -0,0 +1,4 @@
+import { app } from "./index";
+app.listen(3000, () => {
+  console.log("server running at 3000");
+});
---
+++ src/tests/sum.test.ts
@@ -0,0 +1,15 @@
+import { describe, it, expect } from "vitest";
+import { app } from "../index";
+import request from "supertest";
+
+describe("Checking the sum functionality", () => {
+  it("should add 1 and 2 = 3 ", async () => {
+    const res = await request(app).post("/sum").send({
+      a: 1,
+      b: 2,
+    });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual({ answer: 3, id: expect.any(Number) });
+  });
+});
